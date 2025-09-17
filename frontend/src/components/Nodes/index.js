dc6bc23a43945855c983ef08cef0201905197468
Ardhendu Abhishek Meher <meherardhenduabhishek@gmail.com>
2025-09-17 18:08:06 +0530

    Part 1: Node Abstraction completed and Refactor project structure for improved organization and readability

--- frontend/src/components/Nodes/index.js
+++ frontend/src/components/Nodes/index.js
@@ -1,7 +1,7 @@
-// Re-export all available node components from their current locations
-export { InputNode } from '../../nodes/inputNode';
-export { LLMNode } from '../../nodes/llmNode';
-export { OutputNode } from '../../nodes/outputNode';
-export { TextNode } from '../../nodes/textNode';
-export { UppercaseNode, ConcatNode, HttpGetNode, DelayNode, VariableNode } from '../../nodes/extraNodes';
-export { default as makeNode } from '../../nodes/baseNode';
+// Barrel for node components (local to components/Nodes). This avoids depending on src/nodes.
+export { InputNode } from './inputNode';
+export { LLMNode } from './llmNode';
+export { OutputNode } from './outputNode';
+export { TextNode } from './textNode';
+export { UppercaseNode, ConcatNode, HttpGetNode, DelayNode, VariableNode } from './extraNodes';
+export { default as makeNode } from './baseNode';
--- frontend/src/nodes/index.js
+++
@@ -1,7 +0,0 @@
-// Barrel for node components (non-breaking). Allows optional shorter imports.
-export { InputNode } from './inputNode';
-export { LLMNode } from './llmNode';
-export { OutputNode } from './outputNode';
-export { TextNode } from './textNode';
-export { UppercaseNode, ConcatNode, HttpGetNode, DelayNode, VariableNode } from './extraNodes';
-export { default as makeNode } from './baseNode';
